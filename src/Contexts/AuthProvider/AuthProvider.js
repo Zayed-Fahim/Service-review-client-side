d7ce31192c5cf06275c44446cafa349efa4c4da8
Zayed Fahim <zayedfahim33.cse@gmail>
2022-11-10 20:59:12 +0600

    reviews added on db

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,36 +1,68 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import app from '../../Firebase/firebase.config';
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../Firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
-    const logIn = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-    const googleSignInAndLogin = () => {
-        const googleProvider = new GoogleAuthProvider();
-        return signInWithPopup(auth, googleProvider);
-    }
-    const githubSignInAndLogin = () => {
-        const githubProvider = new GithubAuthProvider();
-        return signInWithPopup(auth, githubProvider)
-    }
-    const authInfo = {
-        createUser,
-        logIn,
-        googleSignInAndLogin,
-        githubSignInAndLogin
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleSignInAndLogin = () => {
+    const googleProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleProvider);
+  };
+  const githubSignInAndLogin = () => {
+    const githubProvider = new GithubAuthProvider();
+    return signInWithPopup(auth, githubProvider);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("inside auth state change", currentUser);
+
+      if (currentUser === null || currentUser.emailVerified) {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    createUser,
+    logIn,
+    googleSignInAndLogin,
+    githubSignInAndLogin,
+    logOut,
+    loading,
+    user,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Reviews = () => {
-  const reviews = useLoaderData();
-  const { review } = reviews;
+    const reviews = useLoaderData();
+    const [userReview, setUserReview] = useState('')
+    const [reviewTrigger, setReviewTrigger] = useState(false)
+    
+    const { review, _id } = reviews;
+
+
+    const handleTextAreaTrigger = () => {
+        setReviewTrigger(true);
+        console.log(userReview);
+        //sending user review to db
+        
+        
+
+
+
+    }
+    
+    
+  const handleAddReview = (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const textArea = form.value;
+      setUserReview(textArea)
+    };
+    
+    
   return (
     <div className="flex flex-col">
       <div className="mb-3 xl:w-96">
@@ -13,8 +38,9 @@ const Reviews = () => {
         >
           {review}
         </label>
-        <textarea
-          className="
+        <form action="">
+          <textarea
+            className="
         form-control
         block
         w-full
@@ -31,15 +57,19 @@ const Reviews = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-          id="exampleFormControlTextarea1"
-          rows="3"
-          placeholder="Type your review about this service..."
-        ></textarea>
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Type your review about this service..."
+            onChange={handleAddReview}
+            name="textarea"
+          ></textarea>
+        </form>
       </div>
       <div>
         <button
-          type="button"
-          class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  type="button"
+                  onClick={handleTextAreaTrigger}
+         className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         >
           POST
         </button>
